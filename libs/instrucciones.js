7179994632676e3946c44c83e6ebdc8c55e24c3c
kevin santiago rodriguez gomez <kevin.rodriguezgomez1308@gmail.com>
2025-09-18 19:22:51 -0500

    depurando

--- libs/instrucciones.js
+++ libs/instrucciones.js
@@ -1,10 +1,1 @@
-// Descargar manualmente desde:
-// PDF.js: https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js
-// Mammoth: https://cdnjs.cloudflare.com/ajax/libs/mammoth/1.6.0/mammoth.browser.min.js
-
-// Si quieres usar archivos locales, descarga estos archivos y:
-// 1. Guarda pdf.min.js como libs/pdf.js
-// 2. Guarda mammoth.browser.min.js como libs/mammoth.js
-// 3. Actualiza el index.html para usar rutas locales
-
 console.log('Instrucciones para usar librerías locales en lugar de CDN');
